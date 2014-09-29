722bc69f399b11ea09122fc15dc3e5b665abff4a
luben <luben93@gmail.com>
2014-09-29 14:17:34 +0200

    jdbc
    exemple conn

--- WebShopGit/WebContent/startPage.jsp
+++ WebShopGit/WebContent/startPage.jsp
@@ -36,6 +36,7 @@
    String message = "";
    session.setAttribute( "message", message);
    
+  DB.User user =new DB.User();
   
    %>
    
--- WebShopGit/src/DB/User.java
+++ WebShopGit/src/DB/User.java
@@ -1,5 +1,20 @@
 package DB;
 /*SQL skit*/
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class User {
+	public User(){
+		try {
+	            Class.forName("com.mysql.jdbc.Driver").newInstance();
+	            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+	            	                                   "user=tester&password=blablabla");
+	            System.out.println(conn.getClientInfo());
+	        } catch (Exception ex) {
+	            // handle the error
+	        	System.err.println(ex.getMessage());
+	        }
+	}
+	
 }
